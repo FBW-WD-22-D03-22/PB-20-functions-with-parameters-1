f0750585360abe9a71824e4b0b778c8614bf33b9
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-24 11:43:07 +0100

    done

---
+++ PB-20-functions-with-parameters-enza.js
@@ -0,0 +1,31 @@
+
+//Aufgabenteile 1,2,3,4
+
+
+function meineAddition(firstNum , secondNum , thirdNum){
+    console.log("summe ist:" , firstNum + secondNum + thirdNum);
+}
+
+function meineSubtraktion(firstNum , secondNum , thirdNum){
+    console.log("summe ist:" , firstNum - secondNum - thirdNum);
+}
+
+function meineMultiplikation(firstNum , secondNum , thirdNum){
+    console.log("summe ist:" , firstNum * secondNum * thirdNum);
+}
+
+function meineDivision(firstNum , secondNum , thirdNum){
+    console.log("summe ist:" , firstNum / secondNum / thirdNum);
+}
+
+meineAddition(9 , 18 , 27);
+meineSubtraktion(99 , 54 , 27);
+meineMultiplikation(454 , 333 , 9);
+meineDivision(454 , 333 , 9);
+
+/* Ausgabe im Terminal:
+
+summe ist: 54
+summe ist: 18
+summe ist: 1360638
+summe ist: 0.15148481815148482 */
