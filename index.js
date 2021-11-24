6b8ad3e51acd324160d6fd07b97fdce4adaf0a47
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-24 12:47:16 +0100

    js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function addition( firstNumber , secondNumber , thirdNumber){
-    console.log("addition ergibt:",  firstNumber + secondNumber + thirdNumber);
+    let result1 = firstNumber + secondNumber + thirdNumber;
+    console.log("addition",result1);
 }
 
 function subtraktion( firstNumber , secondNumber , thirdNumber){
-    console.log("subtraktion ergibt:",  firstNumber - secondNumber - thirdNumber);
-}
-
+    let result2 = firstNumber - secondNumber - thirdNumber;
+    console.log("subtraktion", result2);
+    }
 function multiplikation( firstNumber , secondNumber , thirdNumber){
-    console.log("multiplikation ergibt", firstNumber * secondNumber * thirdNumber);
+    let result3 = firstNumber * secondNumber * thirdNumber;
+    console.log("muliplikation",result3);
 }
-
 function division( firstNumber , secondNumber , thirdNumber){
-    console.log("division ergibt", firstNumber / secondNumber / thirdNumber);
+    let result4 = firstNumber/secondNumber/thirdNumber;
+    console.log("division",result4);
 }
-
 addition(7, 9,8);
 subtraktion(8, 3,8);
 multiplikation(6, 30,6);
