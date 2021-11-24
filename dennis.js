48175cc774fd65200362cd3a56ccbee6c2c03d9a
Dennisbullys <d.rochus@hotmail.de>
2021-11-24 11:37:51 +0100

    Dennis lösungen

---
+++ dennis.js
@@ -0,0 +1,29 @@
+console.log("functions-with-parameters");
+
+
+
+// Aufgabenteil 1 :
+function addition(a ,b ,c) {
+  
+  console.log("addition", a + b + c );
+}
+
+// Aufgabenteil 2 :
+function substraktion(a, b, c) {
+  console.log("substraktion", a - b - c);
+}
+
+// Aufgabenteil 3 :
+function multiplikation(a, b, c) {
+  console.log("multiplikation", a * b * c);
+}
+
+// Aufgabenteil 4 :
+function dividieren(a, b, c) {
+  console.log("dividieren", a / b / c);
+}
+
+addition(5,7,9,);
+substraktion(5,7,9,);
+multiplikation(5,7,9,);
+dividieren(5,7,9,);
