019231f407e29737f4283d3f06f3bb52ef72b94b
kawanaza <kawanbabani@gmail.com>
2021-11-24 11:39:00 +0100

    Done

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function Summe (a,b,c) { let Summe = a + b + c;
+    console.log("a + b + c=" , Summe);
+    }
+//let res = a - b - c;
+function minus(a,b,c){ let minus = a - b - c;
+    console.log("a - b - c =" , minus);
+    }
+//let res = a * b * c;
+function Multiplikation(a,b,c){ let Multiplikation = a * b * c;
+    console.log("a * b * c =",Multiplikation);
+    }
+//let res = a / b / c;
+function Division(a,b,c){ let Division = a / b / c;
+    console.log("a / b / c=",Division);
+    }
+
+
+
+Summe(10 ,20 , 30);
+minus(10 ,20 , 30);
+Multiplikation(10 ,20 , 30);
+Division(10 ,20 , 30);
