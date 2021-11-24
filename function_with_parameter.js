bfd13251970b2fcd0cf19a572922a133d27c0bc4
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-24 11:37:27 +0100

    Orhan's lösung

---
+++ function_with_parameter.js
@@ -0,0 +1,22 @@
+
+
+function addition(a, b, c) {
+    console.log("Aufgabe 1) res =", a + b + c)
+}
+
+function subtraktion(a, b, c) {
+    console.log("Aufgabe 2) res  =", a - b - c)
+}
+
+function multiplikation(a, b, c) {
+    console.log("Aufgabe 3) res  =", a * b * c)
+}
+
+function division(a, b, c) {
+    console.log("Aufgabe 4) res  =", a / b / c)
+}
+
+addition(15, 8, 3);
+subtraktion(15, 8, 3);
+multiplikation(15, 8, 3);
+division(15, 8, 3);
