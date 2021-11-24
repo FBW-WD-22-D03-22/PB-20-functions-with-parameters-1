b5b8375dcbe7bb8ef740ce2abb1ae7690069f59f
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-24 11:32:11 +0100

    Function with Parameters

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function addition (a, b, c) {
+    console.log("Ergebnis:" , a + b + c);
+    }
+    
+    
+    function subtraktion (a, b, c) {
+        console.log("Ergebnis:" , a -b -c);
+        }
+    
+    
+    function multiplikation (a, b, c) {
+        console.log("Ergebnis:" , a * b *c);
+        }
+    
+    
+    function division (a, b, c) {
+        console.log("Ergebnis:" , a / b/ c);
+        }
+    
+    
+        division(4 , 6, 12);
+        addition(4 , 6, 12);
+        subtraktion(4 , 6, 12);
+        multiplikation(4 , 6, 12);
+    
