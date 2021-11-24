b6066bc7c747b39b2273c1118434d9e91743ea2f
Serdar68 <Serdarbalkis@outlook.de>
2021-11-24 11:34:16 +0100

    fertig

---
+++ zahlen.js
@@ -0,0 +1,20 @@
+function addition(a, b, c) {
+    console.log("summe ist:", a + b + c)
+}
+
+function subtraktion(a, b, c) {
+    console.log("summe ist:", a - b - c)
+}
+
+function multiplikation(a, b, c) {
+    console.log("summe ist:", a * b * c)
+}
+
+function division(a, b, c) {
+    console.log("summe ist:", a / b / c)
+}
+
+addition(3, 5, 7)
+subtraktion(5, 6, 3)
+multiplikation(1, 0, 12)
+division(8, 5, 2)
