75ace8e47c9d09c561113beb3d207fa7247edd77
sprotte <metatron1972@live.de>
2021-11-24 11:34:01 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,20 @@
+function addition(a, b, c){
+    console.log("addition:", a + b + c);
+}
+
+function subtraktion(a, b, c){
+    console.log("subtraktion:", a - b - c);
+}
+
+function multiplikation(a, b, c){
+    console.log("multiplikation:", a * b * c);
+}
+
+function division(a, b, c){
+    console.log("division:", a / b / c);
+}
+
+addition(2, 10, 6);
+subtraktion(2, 3, 9);
+multiplikation(11, 220, 43);
+division(10, 2, 86);
