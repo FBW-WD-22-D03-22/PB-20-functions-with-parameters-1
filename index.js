f6b9c786f15ace0202ea84ee85eb58608defefed
H94ssein44 <alali.52@yahoo.com>
2021-11-24 11:43:27 +0100

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+addition(22, 100 ,20 );
+subtraktion(22, 33 , 3 );
+multiplikation(10, 30, 40 );
+division(50, 22, 44 );
+
+
+function addition(a, b , c){
+    console.log("addition :", a + b + c);
+}
+
+function subtraktion(a, b, c){
+    console.log("subtraktion :", a - b - c );
+}
+
+function multiplikation(a, b, c ){
+    console.log("multiplikation ", a * b * c );
+}
+
+function division(a, b , c ){
+    console.log("division ergibt", a / b / c );
+}
+
