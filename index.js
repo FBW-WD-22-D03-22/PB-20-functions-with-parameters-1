f65abc4db720752e01f1aafb3e1b9ea09ef67d62
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-24 11:41:41 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,24 @@
+function addition(firstNumber, secondNumber, thirdNumber) {
+    let additionSumme = firstNumber + secondNumber + thirdNumber;
+    console.log("addition ist", additionSumme);
+}
+
+function subtraktion(firstNumber, secondNumber, thirdNumber) {
+    let subtraktionSumme = firstNumber - secondNumber - thirdNumber;
+    console.log("subtraktion ist", subtraktionSumme);
+}
+
+function multiplikation(firstNumber, secondNumber, thirdNumber) {
+    let multiplikationSumme = firstNumber * secondNumber * thirdNumber;
+    console.log("multiplikation ist", multiplikationSumme);
+}
+
+function division(firstNumber, secondNumber, thirdNumber) {
+    let divisionSumme = firstNumber / secondNumber / thirdNumber;
+    console.log("division ist", divisionSumme);
+}
+
+addition(5, 8, 2);
+subtraktion(4, 6, 4);
+multiplikation(8, 7, 7);
+division(10, 6, 8);
