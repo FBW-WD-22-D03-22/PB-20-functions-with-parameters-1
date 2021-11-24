9df918d3b1c7543608e4f43d6bf60b2083240e15
Mazen <mazen.taha88@gmail.com>
2021-11-24 11:47:07 +0100

    My lösung

---
+++ index.js
@@ -0,0 +1,21 @@
+function Addition(a, b, c){
+    console.log("Addition :", a + b + c);
+}
+
+function Subtraktion (a, b, c){
+    console.log("Subtraktion :", a - b - c);
+}
+
+function Multiplikation (a, b, c){
+    console.log ("Multiplikation :", a * b *c);
+}
+
+function Division (a, b, c){
+    console.log("Division :", a/b/c);
+}
+
+
+Addition(5, 15, 5)
+Subtraktion(30, 5, 2)
+Multiplikation(4, 2, 9)
+Division(320, 4, 2)
