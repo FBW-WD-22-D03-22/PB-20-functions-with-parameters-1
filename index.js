0259362b099ccee5d1763a1457ea5c5e1c0a2e12
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-24 11:38:06 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function addition(a , b ,c){
+    console.log("additionSumme ist:" , a + b + c);
+}
+
+
+function subtraktion(a, b , c){
+    console.log("subtraktionSumme ist:", a - b -c);
+}
+
+function multiplikation(a, b ,c){
+    console.log("multiplikationSumme ist", a * b * c);
+}
+
+function division(a, b ,c){
+    console.log("divisionSumme ist", a / b / c);
+}
+
+addition(6 , 7 ,3);
+subtraktion(8, 5, 9);
+multiplikation(10 , 9 , 12);
+division(16 , 18 , 15);
