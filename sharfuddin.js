24359a4735c80c0c8ecda30f898f28f8c14ef4ac
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2021-11-24 11:53:24 +0100

    Done

---
+++ sharfuddin.js
@@ -0,0 +1,20 @@
+function addition(a, b, c) {
+    console.log("summe ist:", a + b + c)
+}
+
+function subtraktion(a, b, c) {
+    console.log("summe ist:", a - b - c)
+}
+
+function multiplikation(a, b, c) {
+    console.log("summe ist:", a * b * c)
+}
+
+function division(a, b, c) {
+    console.log("summe ist:", a / b / c)
+}
+
+addition(7, 6, 7)
+subtraktion(4, 6, 9)
+multiplikation(1, 3, 12)
+division(10, 5, 2)
