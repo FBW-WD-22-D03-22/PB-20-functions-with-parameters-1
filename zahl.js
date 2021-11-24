6516819b3b331accb9ef1fb8c58ea59792b67bf1
slavablack <veaceslav.ciornii@magenta.de>
2021-11-24 11:40:53 +0100

    function

---
+++ zahl.js
@@ -0,0 +1,21 @@
+//1
+function add(erstesumme,zweite,dritte) {
+    console.log(erstesumme+zweite+dritte)
+};
+//2
+function minus(erstesumme,zweite,dritte) {
+    console.log(erstesumme-zweite-dritte)
+};
+//3
+function multi(erstesumme,zweite,dritte) {
+    console.log(erstesumme*zweite*dritte)
+};
+//4
+function divi(erstesumme,zweite,dritte) {
+    console.log(erstesumme/zweite/dritte)
+};
+
+add(5,88,965);
+minus(87,59,3);
+multi(897,0,589);
+divi(87,999,56);
