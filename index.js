3149ccb9516b87071723be5c191b4ca59da4e6bd
Mak513 <osei.makangu@hotmail.com>
2021-11-24 11:36:27 +0100

    osei

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b, c) {
+    console.log("Die Summe lautet:",a+b+c);
+}
+function sub(a, b, c) {
+    console.log("Die Summe lautet:",a-b-c);
+}
+
+function multi(a, b, c) {
+    console.log("Die Summe lautet:",a*b*c);
+}
+
+function divi(a, b, c) {
+    console.log("Die Summe lautet:",a/b/c);
+}
+
+
+add(10,5,89);
+sub(517,55,20);
+multi(7,7,7);
+divi(1579,5,1);
