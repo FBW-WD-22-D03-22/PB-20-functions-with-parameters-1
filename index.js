f5b28cde77f874403ab155ef8e724b0c12845420
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-24 11:42:54 +0100

    js

---
+++ index.js
@@ -0,0 +1,20 @@
+function addition( firstNumber , secondNumber , thirdNumber){
+    console.log("addition ergibt:",  firstNumber + secondNumber + thirdNumber);
+}
+
+function subtraktion( firstNumber , secondNumber , thirdNumber){
+    console.log("subtraktion ergibt:",  firstNumber - secondNumber - thirdNumber);
+}
+
+function multiplikation( firstNumber , secondNumber , thirdNumber){
+    console.log("multiplikation ergibt", firstNumber * secondNumber * thirdNumber);
+}
+
+function division( firstNumber , secondNumber , thirdNumber){
+    console.log("division ergibt", firstNumber / secondNumber / thirdNumber);
+}
+
+addition(7, 9);
+subtraktion(8, 3);
+multiplikation(6, 30);
+division(20, 2);
