11c79151a8783ddbe2ec09d19b8c0bdd3a20b6f5
sham912411 <shamalhakim91@gmail.com>
2021-11-24 11:55:24 +0100

    lösung

---
+++ with parameter.js
@@ -0,0 +1,26 @@
+
+const firstNumber = 50;
+let secondNumber = 12;
+
+
+
+function addition(a, b, c) {
+    console.log("summe ist:", secondNumber + a + b + c = true);
+}
+
+function subtraktion(a, b, c) {
+    console.log("summe ist:", secondNumber - a - b - c =true);
+}
+
+function multiplikation(a, b, c) {
+    console.log("summe ist:", secondNumber * a * b * c = true);
+}
+
+function division(a, b, c) {
+    console.log("summe ist:", secondNumber / a / b / c /= true);
+}
+
+addition(3, 5, 7)
+subtraktion(5, 6, 3)
+multiplikation(1, 0, 12)
+division(8, 5, 2)
