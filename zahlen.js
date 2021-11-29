4b7faa625c5c5d81697f66dba9bdfa7d6ddfd19b
Serdar68 <Serdarbalkis@outlook.de>
2021-11-29 09:08:29 +0100

    update

--- zahlen.js
+++ zahlen.js
@@ -6,6 +6,11 @@ let secondNumber = 15;
 
 function addition(a, b, c) {
     console.log("summe ist:", secondNumber + a + b + c + firstNumber);
+    if ((secondNumber + a + b + c + firstNumber) >49) {
+        console.log("ergebniss richtig")
+    } else {
+        console.log("ergebniss falsch")
+    }
 }
 
 function subtraktion(a, b, c) {
